1a62916501f65d14b9702e159557decc17fde825
Abhishek  Chaudhary <98864225+Abhishekzod007@users.noreply.github.com>
2025-09-10 13:27:07 +0000

    conversionoperations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,33 @@
+let score = undefined
+
+console.log(typeof score)
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "44" => 44
+// "3334dcsd" => NaN
+// true => 1 
+// false=> 0
+
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn =Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+
+
+// 1 => true
+//  0=> false
+// "" => false
+// "djnsd" => true
+
+let number1 = 53
+
+let stringNumber = String(number1)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
