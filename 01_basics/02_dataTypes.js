5c233a37529b54d9072685cad7d03cbb65c6621f
Abhishek  Chaudhary <98864225+Abhishekzod007@users.noreply.github.com>
2025-09-10 13:04:15 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) /// we are suing nodejs , not browser it works only on it
+
+console.log(3
+    +
+    3
+) // code readability should be high
+
+console.log("Jaishree")
+
+let named = "nasc442342"
+let age = 833
+let isLoggedIn = false
+console.log(named);
+
+
+// number => 2 ^ 53
+// bigInt 
+//string => ""
+//boolean => true/false
+// null => standalone value or no value exists
+// undefined => vaue that is not assigned till now
+// symbol => unique
+// typeof undefined  is undefined
+// typeof null is object
+
+// object 
+console.log(typeof undefined);
+console.log(typeof null);
+
