b140e30c8c2b787902ebe34f99db57f7b3097d26
Abhishek  Chaudhary <98864225+Abhishekzod007@users.noreply.github.com>
2025-09-10 11:18:30 +0000

    Learned Let, Const, Var

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 2902332
+let accountEmail = "skjds@google.com"
+var acccountPassword = "33123"
+accountCity = "sjkdn"
+let accountState 
+// accountId = 233
+
+accountEmail = "kjsdcd@dkjs.com"
+acccountPassword = "243234234"
+accountCity = "jaiuedew"
+// Prefer not to use var becuase of issue in block scope and functional scope 
+
+
+console.table([accountEmail, acccountPassword, accountId, accountCity, accountState])
