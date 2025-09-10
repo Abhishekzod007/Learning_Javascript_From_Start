fb4a97622713fdee1ea88b59499a0f996bae5f3a
Abhishek  Chaudhary <98864225+Abhishekzod007@users.noreply.github.com>
2025-09-10 14:15:37 +0000

    comparisions learned

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,18 @@
+//console.table([2>1, 2>=1, 2<1, 2==1, 2!=1])
+
+
+//console.log("2" > 1)
+//console.log("02" > 1)
+
+console.log( null >0)
+console.log(null == 0); // but it id different in case ofequlaity check
+console.log(null >= 0); // when done comparison then it become a number and then thecomparision is drawn 
+
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined < 0);
+
+// these type of comparisions should be avoided
+
+// === checks value and data type also
+console.log("2" === 2);
