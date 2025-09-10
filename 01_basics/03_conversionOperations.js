7878bac2457e3f99908ba18924d59fe84c217940
Abhishek  Chaudhary <98864225+Abhishekzod007@users.noreply.github.com>
2025-09-10 14:02:17 +0000

    operations learned

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,11 +1,11 @@
 let score = undefined
 
-console.log(typeof score)
-console.log(typeof(score));
+//console.log(typeof score)
+//console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
 // "44" => 44
 // "3334dcsd" => NaN
@@ -17,7 +17,7 @@ let isLoggedIn = ""
 
 let booleanIsLoggedIn =Boolean(isLoggedIn)
 
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 
 // 1 => true
@@ -28,6 +28,51 @@ console.log(booleanIsLoggedIn);
 let number1 = 53
 
 let stringNumber = String(number1)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
 
+
+
+
+// ******************************* Operations **************************
+
+
+
+
+let value = 3
+let negValue = -value
+
+//console.log(negValue);
+
+
+//console.table([2+2, 2-2, 2*2, 2**3, 2/3, 2%3])
+
+
+let str1 = "hello"
+let str2 = " there"
+
+let str3 = str1 + str2
+
+//console.log(str3)
+
+//console.log("1" + 2)
+//console.log(1+ "2");
+//console.log("1"+2 +2);
+//console.log(1+2+"2");
+
+// it all has given in ECMASCRIPT defines standard how the priority works
+// so these are only good forthe exams not for the corporate so readability should be high
+//console.log((3+4)*5%3);
+
+//console.log(+true);  //1
+//console.log(+"");    //0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter++);
+
+console.log(gameCounter);
