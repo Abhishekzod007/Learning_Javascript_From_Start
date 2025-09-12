46160654c4eca973b6a69946eec3c65ffa79f48c
Abhishek  Chaudhary <98864225+Abhishekzod007@users.noreply.github.com>
2025-09-12 12:42:11 +0000

    Learning about objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,49 @@
+// singleton
+//Object.create is a constructor method to create obj
+
+// Object literals
+
+const mySym = Symbol("key1")
+
+const  JsUser = {
+    name: "Abhi",
+    "full name": "Abhishek Chaudhary",
+    age: 20,
+    [mySym]:"Mykey1",
+    location: "Rawatsar, Hanumangarh",
+    email: "abhishek@sarv.ai.com",
+    isLoggedIn: false,
+    LastLoggedInDays: ["Monday", "Tuesday"]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"]);
+// console.log(JsUser["full name"]);
+// console.log( typeof JsUser[mySym]);
+
+
+
+
+//console.log(Object.getOwnPropertySymbols(JsUser)); // Shows an array of symbol keys
+
+
+JsUser.email = "abhishek@chatgpt.com"
+//Object.freeze(JsUser)
+JsUser.email = "abhsiehk@google.com"
+//console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("hello bhai kitna kam kiya aaj");
+    
+}
+
+JsUser.greeting_two = function(){
+    console.log(`hello bhai ${this.name} kitna kam kiya aaj`);
+    
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greeting_two());
+
+
+
