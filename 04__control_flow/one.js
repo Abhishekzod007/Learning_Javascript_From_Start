329f517a6693435f4d652d12794b083bb4a517e0
Abhishek  Chaudhary <98864225+Abhishekzod007@users.noreply.github.com>
2025-09-13 10:15:56 +0000

    Learning  Control  Flow in javacript

---
+++ 04__control_flow/one.js
@@ -0,0 +1,56 @@
+//if
+// const isUserLoggedIn = true
+// const temp = 39
+
+// if (temp === 40){
+//    console.log("less then 40");
+// }  else{
+//     console.log("temperature is greater than 40");
+    
+// }
+
+// < , > , <=, >=, ==, !=, === , !==
+
+
+
+const score = 100
+
+// if(score >50){
+//     const power ="fly"
+//     console.log(`User power: ${power}`);
+    
+// }
+// console.log(`User power: ${power}`);
+
+const balance = 1000
+// if(balance>500) console.log("test"), console.log("test2"); // ye nhi krna h
+
+// if(balance < 500){
+//      console.log("less than");
+     
+// }else if(balance <750){
+//     console.log("less than 750");
+    
+// } else if(balance <900){
+//     console.log("less then 900");
+    
+// }else {
+//     console.log("less than 1200");
+    
+// }
+
+
+const UserLoggedIn = true
+const debitCard = true
+const LoggedInFromGoogle = false
+const LoggedInFromEmail  = true
+
+if(UserLoggedIn && debitCard && 2==3){
+    console.log("Allow to buy courses");
+    
+}
+
+if(LoggedInFromEmail || LoggedInFromGoogle ){
+    console.log("User Logged In");
+    
+}
