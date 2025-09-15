d626561966ff4d512d6a78ee3c8cf5a26bbca312
Abhishek  Chaudhary <98864225+Abhishekzod007@users.noreply.github.com>
2025-09-15 04:44:18 +0000

    Different types of loops

---
+++ 05_iterations/five.js
@@ -0,0 +1,41 @@
+const coding = ["js","ruby","java","python","cpp"]
+
+coding.forEach( function (item) {
+    //console.log(item);
+    
+} )
+
+coding.forEach(  (item) =>{
+//console.log(item);
+
+})
+
+function printMe(item){
+    //console.log(item);
+    
+}
+coding.forEach(printMe)
+
+
+//coding.forEach((item, index, arr)=>{console.log(item, index, arr);
+//})
+
+const MyCode = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    }
+]
+
+MyCode.forEach((item)=>{
+    console.log(item.languageName);
+    
+})
