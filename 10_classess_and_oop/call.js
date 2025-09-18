8cbbf3e271bd708b315ae5a929fb7f0cf4e29990
Abhishek  Chaudhary <98864225+Abhishekzod007@users.noreply.github.com>
2025-09-18 06:03:01 +0000

    Call in detail

---
+++ 10_classess_and_oop/call.js
@@ -0,0 +1,16 @@
+function SetUserName(username){
+    // complex DB calls 
+    console.log("called");
+    
+    this.username = username
+}
+
+function createUser(username, email, password){
+    SetUserName.call(this,username)
+    this.email= email
+    this.password = password
+}
+
+const chai = new createUser("chai", "chai@fb.com", "123")
+console.log(chai);
+// call :-  current execution context pass pr detrahdusre function me 
